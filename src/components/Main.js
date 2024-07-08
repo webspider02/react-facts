cd629d738319510a4f2e476397a425eeb3f58948
giorgoszampakikas@gmail.com <134231282+ymnoos01@users.noreply.github.com>
2024-07-08 19:01:55 +0300

    Navbar & Main setup

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Main goes here</h1>
+
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li> 
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="logo192.png" alt="logo" />
+
+                <h1>ReactFacts</h1>
+
+                <h3>React Course - Project 1</h3>
+            </nav>
+        </header>
+
+    );
+}
